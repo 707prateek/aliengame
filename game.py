f86406a339cd37b29a85f1efc39a00c6d284cded
prateek <prateekmaurya707@gmail.com>
2021-09-12 22:07:04 +0530

    learning pygame by making any game

--- game.py
+++ game.py
@@ -1,1 +1,8 @@
-from pygame import *
+import pygame
+#This line is important to run our window
+screen=pygame.display.set_mode((1200,600))
+running=True
+while running:
+    ask=input("if you want to quit type q and enter")
+    if ask=='q':
+        running=False
