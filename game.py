64ace84fd10cc3a18e55932204807b6f7ee765c9
prateek <prateekmaurya707@gmail.com>
2021-09-12 22:13:28 +0530

    learning pygame by making any game

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 import pygame
 #This line is important to run our window
 screen=pygame.display.set_mode((1200,600))
+
+#FOR WINDOW NOT DISAPPERING WE MADE A LOOP
 running=True
 while running:
-    ask=input("if you want to quit type q and enter")
-    if ask=='q':
-        running=False
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running==False
